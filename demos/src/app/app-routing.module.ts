1dd348e41f33f2ef6edaa6a17b047653ef1bfa81
gouravkanker <gouravkanker444@gmail.com>
2024-04-01 16:53:11 +0530

    add changes

--- demos/src/app/app-routing.module.ts
+++ demos/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransationComponent } from './transation/transation.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: TransationComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
